2e72d6e12ab8b5c293e9af65501b0d9133070094
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2023-06-23 22:40:47 +0600

    Update VersioningPersonController.java

--- VersioningRestAPI/src/main/java/com/apiproject/restapiservice/restfulwebservice/VersioningPersonController.java
+++ VersioningRestAPI/src/main/java/com/apiproject/restapiservice/restfulwebservice/VersioningPersonController.java
@@ -2,7 +2,7 @@ package com.apiproject.restapiservice.restfulwebservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//versioning Rest API  ... ( mapping  2 ta)
 @RestController
 public class VersioningPersonController {
     @GetMapping("/v1/person")
